9fb3704a20df29a12eba279e5f85f70af6be65ac
Marcos Almeida <marcos@marcosalmeida.dev>
2021-01-07 15:44:41 +0100

    fest: add customer endpoints

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -2,7 +2,9 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,5 +23,27 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public CustomerDto getCustomer(@PathVariable("customerId") UUID customerId) {
         return customerService.getCustomerById(customerId);
+    }
+
+    @PostMapping
+    public ResponseEntity createBeer(@RequestBody CustomerDto customerDto) {
+        var savedCustomer = customerService.createCustomer(customerDto);
+
+        var headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedCustomer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping({"/{customerId}"})
+    public void handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto beerDto) {
+        customerService.updateCustomer(customerId, beerDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping({"/{customerId}"})
+    public void deleteBeer(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteCustomer(customerId);
     }
 }
